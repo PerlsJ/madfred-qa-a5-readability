2250193f3e76f78d0fa4133ae04302a8bef6b3e5
Jacob <Jperley@redhotlearning.com>
2013-09-12 13:15:31 -0400

    Hi Clyde!!!

--- src/DamageThisOne.java
+++ src/DamageThisOne.java
@@ -3,36 +3,28 @@ import java.util.Scanner;
 public class DamageThisOne {
 
 	/**
-	 * @author Clyde Brown
-	 * @version 1.0
-	 * @date August 12, 2013
-	 * @param void
+	 * @author Charlie Brown
+	 * @version 9.0
+	 * @date April 20, 3073
+	 * @param void static main String()
 	 */
 	
 	public static void main(String[] args) {
-		//This section declares variables and instantiates the Scanner sc
-		int ticketNumber, stem, checkDigit;
-		Scanner sc = new Scanner(System.in);
+		//Scan this 
+		int ticketNumber, stem, checkDigit;Scanner sc = new Scanner(System.in);
 		
-		/* This section requests input from the user, in the form of a 
-		 * six digit number, then it generates a stem and checkdigit 
-		 * from that number
+		/*Blah blah blah parameter blah blah blah
 		 */
-		System.out.print("Please enter a six-digit ticket number: ");
-		ticketNumber = sc.nextInt();
+		System.out.print("Please enter a six-digit ticket number: ");ticketNumber = sc.nextInt();
 		stem = ticketNumber / 10;
-		checkDigit = ticketNumber % 10;	
+checkDigit = ticketNumber % 10;	
 
-		/* This section will check the validity of the ticket number
-		 * and continue to prompt the user for a new ticket number
-		 * while the current number is invalid
+		/* This is how clyde write his code!!
 		 */
 		while (checkDigit != stem % 7) {
-			System.out.println("That is NOT a valid ticket number, bucko!");
-			System.out.print("Try again: ");
-			ticketNumber = sc.nextInt();
-			stem = ticketNumber / 10;
-			checkDigit = ticketNumber % 10;	
+				System.out.println("That is NOT a valid ticket number, bucko!");System.out.print("Try again: ");
+				ticketNumber = sc.nextInt();stem = ticketNumber / 10;
+		checkDigit = ticketNumber % 10;	
 		}
 						
 		System.out.println("That is a valid ticket number, well done!");
--- src/FixThisOne.java
+++ src/FixThisOne.java
@@ -4,28 +4,35 @@ public class FixThisOne {
 
 	public static void main(String[] args) 
 	{
-		//This program checks your airline tickets
-		 //This is a bynch of code dunno what it doez
-		final float PRICE_PER_DOZEN = 3.25f;final float PRICE_PER_SINGLE = 0.45f;
-		int eggs, d, s;
-			float dozens_cost, singlesCost, tc;
+		//This is where your variables are declared
+		
+		final float PRICE_PER_DOZEN = 3.25f;
+		final float PRICE_PER_SINGLE = 0.45f;
+		int eggs, dozen, singles;
+		
+		
+		//This will prompt the user for information
+		float dozens_cost, singlesCost, totalCost;
 		Scanner kbrd = new Scanner(System.in);
 		System.out.print("How many eggs do you want? >> ");
 		eggs = kbrd.nextInt();
-		d = eggs / 12;
-		s = eggs % 12;
-		dozens_cost = d * PRICE_PER_DOZEN;
-		singlesCost = s * PRICE_PER_SINGLE;
-		tc = dozens_cost + singlesCost;
+		dozen = eggs / 12;
+		singles = eggs % 12;
 		
-		/*This program checks your airline tickets
-		 * This is a block comment
-		 * Coding is fun!
+		/*
+		 * This part of the program will calculate the price
+		 * of your eggs both singles and doubles
 		 */
+		dozens_cost = dozen * PRICE_PER_DOZEN;
+		singlesCost = singles * PRICE_PER_SINGLE;
+		totalCost = dozens_cost + singlesCost;
+		
+		//This part of the program will output the results and cost of the eggs
+		 
 		System.out.println("You ordered " + eggs + " eggs in total.");
-			System.out.println("That's " + d + " dozen at $3.25, or $" + dozens_cost);
-				System.out.println("That's " + s + " singles at 45c, or $" + singlesCost);
-				System.out.println("Your total cost is $" + tc);
+		System.out.println("That's " + dozen + " dozen at $3.25, or $" + dozens_cost);
+		System.out.println("That's " + singles + " singles at 45c, or $" + singlesCost);
+		System.out.println("Your total cost is $" + totalCost);
 		
 	
 	}
